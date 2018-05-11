9be5e8b977ccd15ac6c29c0ac9e950be5a5fa9e8
Sean Grogan <sean.grogan@gmail.com>
2018-05-11 10:59:21 -0400

    Separated out GetPoit()

--- pi_estimator.py
+++ pi_estimator.py
@@ -12,12 +12,15 @@ this is at the end of the script.
 def circle_estimator(n):
     inside = 0
     for k in range(n):
-        x = random.random()
-        y = random.random()
-
-        if math.sqrt(x**2+y**2) <= 1:
+        if GetPoint():
             inside += 1
     return 4*inside/n
+
+
+def GetPoint():
+    # Returns True if the point is inside the circle.
+    x, y = random.random(), random.random()  # uniform [0.0, 1.0)
+    return math.sqrt(x ** 2 + y ** 2) <= 1
 
 if __name__ == "__main__":
     n1 = 10
