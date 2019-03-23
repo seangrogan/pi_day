d31f781552094e349beb64d4ee8f9ef4acfe540a
Sean Grogan <sean.grogan@gmail.com>
2019-03-23 10:43:06 -0400

    Micro-optimized the coprimes pi generator

--- pi_from_coprimes.py
+++ pi_from_coprimes.py
@@ -1,37 +1,28 @@
 import argparse, time
+
 try:
     from numpy.random import randint as rnd
-    from numpy import gcd, sqrt, pi
+    from numpy import gcd, sqrt, pi as pie
 except ImportError:
     from random import randint as rnd
-    from math import gcd, sqrt, pi
+    from math import gcd, sqrt, pi as pie
 
 
-def pi(max_int=120, n=500):
-    """ Computes pi with coprimes.
-    :param max_int: upper bound of the random integers
-    :param n: gets the number of trials to execute
-    :return: Estimate of pi
-    """
-    return sqrt(6/(sum([1 for i in range(n) if 1 == gcd(rnd(1, max_int), rnd(1, max_int))]) / n))
+def pi(max_int=256 ** (2 * 36), n=500):
+    return sqrt(6 / (sum(1 == gcd(rnd(1, max_int), rnd(1, max_int)) for _ in range(n)) / n))
 
 
-def _pi_from_coprimes():
+def pi_from_coprimes():
     """ Does an experiment to calculate pi with coprimes
     """
-    t=time.time()
+    t = time.time()
     args = _arg_parse()
-    cp_count = sum([1 if 1 == gcd(rnd(1, args.m_int), rnd(1, args.m_int))
-                    else 0
-                    for i in range(args.n)])
-    pi_estimate = sqrt(6/(cp_count/args.n))
-    print('Coprime Count  \t{c} of {n} ({pct:.2%})'.format(c=cp_count,
-                                                           n=args.n,
-                                                           pct=(cp_count/args.n)
-                                                           ))
-    print('π Estimate     \t{}'.format(pi_estimate))
-    print('Pct Difference \t{pct:.2%}'.format(pct=(pi_estimate-pi)/(pi_estimate+pi)))
-    print('Time           \t{:.2f} sec'.format(time.time()-t))
+    cp_count = sum((1 == gcd(rnd(1, args.m_int), rnd(1, args.m_int))) for _ in range(args.n))
+    pi_estimate = sqrt(6 / (cp_count / args.n))
+    print('Coprime Count  \t{c} of {n} ({pct:.2%})  '.format(c=cp_count, n=args.n, pct=(cp_count / args.n)))
+    print('π Estimate     \t{}                      '.format(pi_estimate))
+    print('Pct Difference \t{pct:.2%}               '.format(pct=(pi_estimate - pie) / (pi_estimate + pie)))
+    print('Time           \t{:.2f} sec              '.format(time.time() - t))
 
 
 def _arg_parse():
@@ -40,7 +31,7 @@ def _arg_parse():
                         type=int,
                         help='Number of trials, default=500',
                         default=500)
-    parser.add_argument('-m_int', '-max_int','-m',
+    parser.add_argument('-m_int', '-max_int', '-m',
                         type=int,
                         help='Maximum Random Integer, default=120',
                         default=120)
@@ -48,6 +39,6 @@ def _arg_parse():
 
 
 if __name__ == '__main__':
-    _pi_from_coprimes()
+    pi_from_coprimes()
 else:
     pass
--- test.py
+++ test.py
@@ -3,6 +3,6 @@ import math as m
 import pi_from_coprimes as pfc
 import pi_from_random_numbers as pfr
 
-print('pi_from_coprimes       : {}'.format(pfc.pi(n=1000000, max_int=50000)))
-print('pi_from_random_numbers : {}'.format(pfr.pi(n=1000000)))
+print('pi_from_coprimes       : {}'.format(pfc.pi(n=1000, max_int=50000)))
+print('pi_from_random_numbers : {}'.format(pfr.pi(n=1000)))
 print('pi from math           : {}'.format(m.pi))
